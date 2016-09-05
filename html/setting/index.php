62d745d4abc58b59e016696f6917d2a3d87d9edb
akuzo <ubuntu@cotuvideo.com>
2016-09-05 09:08:52 +0900

    index.html 修正

--- html/setting/index.php
+++ html/setting/index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<html>\n";
 echo "<head>\n";
-echo '"meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">";
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">";
 echo "<title>setting</title>";
 echo "</head>\n";
 echo "<body>\n";
