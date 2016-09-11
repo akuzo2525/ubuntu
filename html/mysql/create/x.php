da2b2a688c4176de0fc327f3df82fd4f70799c2b
akuzo <akuzo@cotuvideo.sakura.ne.jp>
2016-09-12 02:38:38 +0900

    ;が抜けてたのを修正

--- html/mysql/create/x.php
+++ html/mysql/create/x.php
@@ -2,7 +2,7 @@
 
 $url="localhost";
 $usr="nico";
-$host="%";
+$host="llocalhost";
 $pass="";
 
 mysql_connect('localhost','root','') or dir(mysql_error());
@@ -10,7 +10,7 @@ mysql_connect('localhost','root','') or dir(mysql_error());
 $query="GRANT ALL PRIVILEGES ON *.* TO '$usr'@'$host' WITH GRANT OPTION";
 echo "$query\n";
 $result=mysql_query($query) or die(mysql_error());
-echo "result=$result\n"
+echo "result=$result\n";
 
 mysql_close() or dir(mysql_error());
 
